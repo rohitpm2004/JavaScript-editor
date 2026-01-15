bd3b06ecb37ad6ee5f5b0dc4ef6d15ccf7c6b867
Asiya Farhath <asiyacode@gmail.com>
2026-01-15 19:22:34 +0530

    fix cors configuration

--- Backend/server.js
+++ Backend/server.js
@@ -3,7 +3,7 @@ import app from "./src/app.js";
 import connectDB from "./src/config/db.js";
 dotenv.config();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port http://localhost:${PORT}`);
