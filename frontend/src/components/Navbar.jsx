480ebdf23a3c33a4d168009d56c1657d0a89849a
rohitpm2004 <kirsagarmenaka@gmail.com>
2026-02-04 16:50:46 +0530

    Update logo import in Navbar component

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import "./navbar.css"
 import { FaSmile } from "react-icons/fa";
+import logo from "../assets/logo.png"
 // import logo from "\barabari_logo-CW6k3Oea.png"
 const Navbar = ( {theme} ) => {
   return (
     <div style={{backgroundColor:theme==="dark"?"#101828":"#3949AB" , color:theme==="dark"?"white":"white"}} className='navbar'>
       <div className="left">
-        <img src="\src\assets\logo.png"   alt="" />
+        <img src={logo} alt="Logo"/>
         <div className="wrap-heading">
         <h2 className='nav-h2'>the barabari Collective</h2>
         <p className='nav-p'  >Javascript online Compiler</p>
